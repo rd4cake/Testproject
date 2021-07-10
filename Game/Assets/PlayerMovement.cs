d4a7936764b93b6c9f9aa89e2b439b372c712443
rd4cake <dennywang2003@gmail.com>
2021-07-10 13:59:05 -0400

    moved game to a different folder

---
+++ Game/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D mController;
+
+    public float mRunSpeed;
+    private float mHorizontalMovement;
+    private bool mJump;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mHorizontalMovement = Input.GetAxisRaw("Horizontal")*mRunSpeed;
+        if (Input.GetButtonDown("Jump")) {
+            mJump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+        mController.Move(mHorizontalMovement*Time.fixedDeltaTime,false,mJump);
+        mJump = false;
+    }
+}
